ea8c2b172e17d42c29e9a99ab02d39ba0e484c7f
Srinivas G <srinu.gandikota@gmail.com>
2025-11-06 23:46:31 +0530

    Different Ways to Declare Variables

--- 02. Variables/02_multiple_vars/02_multiple_vars.go
+++ 02. Variables/02_multiple_vars/02_multiple_vars.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// Explicitly declaring variable type and assigning value
+	var coffee_name string = "Espresso"
+
+	// Type inference - Go infers the type based on the assigned value
+	var coffee_price = 2.50
+
+	// Using short variable declaration for size. This can only be used inside functions.
+	size := "Medium"
+	fmt.Println(size, coffee_name, "price is $", coffee_price)
 }
