bf2f3e58afeb02915fae5cb92984b14ba31e71f3
Srinivas G <srinu.gandikota@gmail.com>
2025-11-10 07:09:21 +0530

    Declaring Multiple Variables in One Line

---
+++ 02. Variables/09_multiple_declaration/09_multiple_declaration.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var coffee, milk, sugar, isReady = 1, 2, 3, true
+	// var coffee = 1
+	// var milk = 2
+	// var sugar = 3
+
+	fmt.Println("coffee:", coffee, "Milk:", milk, "Sugar:", sugar, "IsReady:", isReady)
+}
